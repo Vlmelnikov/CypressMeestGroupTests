fdf7508df827d8bab364feb44004c04667383544
IssueCreatorCircle335 <Lekterwm@gmail.com>
2021-11-14 22:39:56 +0200

    Actions generalization

--- cypress/integration/MeestGroupTests/meest-group_loginform.spec.js
+++ cypress/integration/MeestGroupTests/meest-group_loginform.spec.js
@@ -5,15 +5,21 @@ context('Login form tests', () => {
 
 	it('Success login', () => {
 		//Arrange
-		const email = 'formeestgrouptest+1002@gmail.com'
+		const email = 'formeestgrouptest@gmail.com'
 		const pass = 'A1234567'
 
 		//Act
-		cy.get('input[type=text]').type(email)
-		cy.get('input[type=password]').type(pass)
+		loginWithEmailAndPassword(email, password)
 		cy.contains('Login').click()
 
 		//Assert
 		cy.contains('Please enter tracking number').should('be.visible')
 	})
+
+	//Similar function could be exstracted to be reused in different test cases. Depends of project structure.
+	function loginWithEmailAndPassword(email, password) {
+	    cy.get('input[type=text]').type(email)
+	    cy.get('input[type=password]').type(password)
+	    cy.contains('Login').click()
+	}
 })
